0d8e5ffae72c29171be028b9a7a81bbbba024971
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-01-27 13:32:49 -0600

    initial commit/UpdateMovie started

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const initialState = {
+    id: '',
+    title: '',
+    director: '',
+    metascore: '',
+    stars: []
+}
+
+function UpdateMovie() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UpdateMovie
